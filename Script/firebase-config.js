97ccd30d0caa4560ae0517056e8f4c291a3216fc
DoMinhTuong <dominhtuong08@gmail.com>
2026-03-08 17:14:15 +0700

    3

--- Script/firebase-config.js
+++ Script/firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDGjQtwdnQd1DZfwWBtKI6c33K2k6gC1iw",
-  authDomain: "finalproject567321.firebaseapp.com",
-  projectId: "finalproject567321",
-  storageBucket: "finalproject567321.firebasestorage.app",
-  messagingSenderId: "732290172695",
-  appId: "1:732290172695:web:29f26286426db629f5d952",
-  measurementId: "G-11E10LP4RV"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: ".",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
