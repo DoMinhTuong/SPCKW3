dad460f002b9ba6dc162d4e28132315cec3d67bd
DoMinhTuong <dominhtuong08@gmail.com>
2026-03-15 17:58:14 +0700

    edse

---
+++ Script/mainmenu.js
@@ -0,0 +1,40 @@
+const auth = firebase.auth();
+
+/* Protect page */
+
+auth.onAuthStateChanged(user => {
+
+if(!user){
+window.location.href = "login.html";
+}
+
+});
+
+
+/* Buttons */
+
+document.getElementById("homeBtn").onclick = () => {
+location.reload();
+};
+
+document.getElementById("searchBtn").onclick = () => {
+window.location.href = "song.html";
+};
+
+document.getElementById("playlistBtn").onclick = () => {
+alert("Playlist feature coming soon");
+};
+
+document.getElementById("profileBtn").onclick = () => {
+alert("Profile feature coming soon");
+};
+
+document.getElementById("logoutBtn").onclick = () => {
+
+auth.signOut().then(() => {
+
+window.location.href = "login.html";
+
+});
+
+};
