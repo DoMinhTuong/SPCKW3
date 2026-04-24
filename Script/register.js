9c8267b30aa3e237d718530a6c6ea8eb705a403a
DoMinhTuong <dominhtuong08@gmail.com>
2026-04-24 23:11:58 +0700

    Login check

--- Script/register.js
+++ Script/register.js
@@ -63,3 +63,15 @@ form.addEventListener("submit", async (e) => {
 
   });
 });
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth();
+
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+        // Nếu chưa đăng nhập
+        window.location.href = "login.html";
+    }
+
+});
